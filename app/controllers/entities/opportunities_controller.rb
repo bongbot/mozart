d69977401e9c6d6f2e8be5629419acc4ea0c7769
Tran Manh Cuong <tcuong1829@gmail.com>
2016-04-11 01:21:46 +0700

    useing flash instead of params for related assest

--- app/controllers/entities/opportunities_controller.rb
+++ app/controllers/entities/opportunities_controller.rb
@@ -33,14 +33,12 @@ class OpportunitiesController < EntitiesController
   # GET /opportunities/new
   #----------------------------------------------------------------------------
   def new
-    logger.debug "TTT: : #{flash[:tryhard].inspect}    /**#{__FILE__}:#{__LINE__}"
-
     @opportunity.attributes = { user: current_user, stage: Opportunity.default_stage, access: Setting.default_access, assigned_to: nil }
     @account     = Account.new(user: current_user, access: Setting.default_access)
     @accounts    = Account.my.order('name')
 
-    if params[:related]
-      model, id = params[:related].split('_')
+    if flash[:related]
+      model, id = flash[:related].split('_')
       if related = model.classify.constantize.my.find_by_id(id)
         instance_variable_set("@#{model}", related)
         @account = related.account if related.respond_to?(:account) && !related.account.nil?
