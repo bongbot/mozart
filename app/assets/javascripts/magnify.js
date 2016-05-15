8a218e4ba1fa40c7acc5db8e0257faa69e3d7f5e
Tran Manh Cuong <tcuong1829@gmail.com>
2016-05-15 13:47:17 +0700

    update magnify

--- app/assets/javascripts/magnify.js
+++ app/assets/javascripts/magnify.js
@@ -11,19 +11,26 @@ function jqueryUI(html){
         width:"auto"
     });
 }
+
+MAGNIFY.bodyScrollable = function(scrollable){
+    var css = scrollable ? "visible" : "hidden";
+    $("body").css("overflow", css);
+}
+
+
 MAGNIFY.openModal = function(html){
     //remove modal if exist
     if($("#magnify-background-layer")){
         $("#magnify-background-layer").remove();
     }
 
-    console.log("TTT: " + "open modal");
     var backgroundLayer = $("<div class='background-layer' id='magnify-background-layer'> </div>");
     backgroundLayer.appendTo($("body"));
     var center = $("<div id='magnify-center' class='magnify-center'></div>");
     center.appendTo(backgroundLayer);
+    center.css("overflow", "auto");
+    center.css("border-radius", "10px");
     $(html).appendTo(center);
-    center.offset({left: ($(window).width() - center.width())/2, top: ($(window).height() - center.height())/2})
 
     $("#magnify-background-layer").click(function(e){
         var target = $(e.target);
@@ -31,12 +38,43 @@ MAGNIFY.openModal = function(html){
             $("#magnify-background-layer").remove();
         }
     });
+
+    $(window).resize(function(){
+        resizeMagnifyCenter();
+    });
+
+    var paddingH = 20;
+    var paddingW = 20;
+
+    function resizeMagnifyCenter(){
+        var contentH = center[0].scrollHeight;
+        var contentW = center[0].scrollWidth;
+        var maxH = $(window).height() - paddingH * 2;
+        var maxW = $(window).width() - paddingW * 2;
+
+        if(contentH > maxH) {
+            center.height(maxH)
+        }
+
+        if(contentW > maxW) {
+            center.width(maxW);
+        }
+
+        center.offset({left: ($(window).width() - center.width())/2, top: ($(window).height() - center.height())/2})
+
+    }
+
+    resizeMagnifyCenter();
+
+    MAGNIFY.bodyScrollable(false);
+
 }
 
 MAGNIFY.closeModal = function(html){
     if($("#magnify-background-layer")){
         $("#magnify-background-layer").remove();
     }
+    MAGNIFY.bodyScrollable(true);
 }
 
 
